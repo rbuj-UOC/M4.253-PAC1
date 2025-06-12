fb0a98c8e7d2980282cf92f43cd5d4388654f530
rbuj <robert.buj@gmail.com>
2025-06-12 09:07:19 +0200

    honor: npm create vite@latest

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+npm create vite@latest
+npm install --save-dev eslint-config-prettier eslint-config-recommended prettier
+npm install --save-dev jest @testing-library/react @testing-library/jest-dom jest-environment-jsdom @testing-library/user-event
+npm install --save-dev babel-jest @babel/preset-env @babel/preset-react
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+}
+
+export function ex1(value) {
+  return value < 0;
+}
+
+export function ex2(price, amount) {
+  return price * amount;
+}
+
+export default App;
