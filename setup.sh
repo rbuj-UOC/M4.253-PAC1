e49118465ca37cbdedf66500fdd33d219feb935d
rbuj <robert.buj@gmail.com>
2025-06-12 09:31:04 +0200

    jest -> jest --watchAll

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ npm create vite@latest
 npm install --save-dev eslint-config-prettier eslint-config-recommended prettier
 npm install --save-dev jest @testing-library/react @testing-library/jest-dom jest-environment-jsdom @testing-library/user-event
 npm install --save-dev babel-jest @babel/preset-env @babel/preset-react
+npm install --save-dev whatwg-fetch
